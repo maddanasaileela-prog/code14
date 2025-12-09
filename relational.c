4d15210fa2bdce11728d44706000580cb917528a
maddanasaileela-prog <maddanasaileela@gmail.com>
2025-12-09 22:59:15 +0530

    Add files via upload

---
+++ relational.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main() {
+    int a=20,b=30;
+    // Write C code here
+    printf("%d",a<b);
+     printf("%d",a>b);
+      printf("%d",a=b);
+       printf("%d",a<=b);
+
+    return 0;
+}
